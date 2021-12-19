d133b83e166f718b98c9d2d456199b3a2f54b920
Daniel Diaz <diaz.carrete@facebook.com>
2021-12-19 17:38:56 +0100

    Added ResourceKey.

--- lib/Dep/Loader.hs
+++ lib/Dep/Loader.hs
@@ -1,10 +1,41 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Dep.Loader where
 
 import GHC.Generics qualified as G
 import Data.ByteString 
+import Data.Monoid
+import GHC.TypeLits (KnownSymbol, symbolVal)
+import Data.Proxy
+import Data.List.Split
+
+type DatatypeName = String
+
+type ModuleName = String
+
+data ResourceKey = ResourceKey [ModuleName] DatatypeName deriving (Show, Eq, Ord)
 
 newtype Loader m = Loader {
-        loadE :: m (Maybe ByteString)
+        loadE :: ResourceKey -> m (Maybe ByteString)
     } deriving G.Generic
+
+instance Monad m => Semigroup (Loader m) where
+  l1 <> l2 = Loader \resourceKey -> do
+    mb <- loadE l1 resourceKey
+    case mb of 
+      Nothing -> loadE l2 resourceKey
+      Just b -> pure (Just b)
+
+instance Monad m => Monoid (Loader m) where  
+    mempty = Loader \_ -> pure Nothing
+
+resourceKey :: forall a name mod p nt x . (G.Generic a, G.Rep a ~ G.D1 ('G.MetaData name mod p nt) x, KnownSymbol name, KnownSymbol mod)  => ResourceKey
+resourceKey = ResourceKey (splitOn "." (symbolVal (Proxy @mod))) (symbolVal (Proxy @name))
